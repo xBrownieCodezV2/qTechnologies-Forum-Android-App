1539473cedb3b51b33d3c66546f5487bafa3d855
xBrownieCodez <72617433+xBrownieCodezV2@users.noreply.github.com>
2022-03-11 00:20:18 +0100

    Initial commit

---
+++ app/src/main/java/dev/mdma/forum/app/DelegationService.java
@@ -0,0 +1,18 @@
+package dev.mdma.forum.app;
+
+
+import com.google.androidbrowserhelper.locationdelegation.LocationDelegationExtraCommandHandler;
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+            registerExtraCommandHandler(new LocationDelegationExtraCommandHandler());
+        
+    }
+}
+
